92a75ec89cf05c99a514b9dcb85bf25d6f476879
Brian Kelly <polymonic@gmail.com>
2016-12-23 15:18:35 -0500

    Add Maven blog post and fix Markdown support

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem 'middleman-livereload'
 gem 'middleman-compass', '>= 4.0.0'
 gem 'middleman-s3_sync', '4.0.3'
 gem 'middleman-blog'
+gem 'middleman-syntax'
 
 gem 'mime-types'
+gem 'redcarpet'
 
--- config.rb
+++ config.rb
@@ -1,9 +1,12 @@
 activate :directory_indexes
 
+activate :syntax
+ignore 'source/stylesheets/site.css.erb.scss'
+
 activate :blog do |blog|
   blog.prefix = "blog"
+  blog.layout = "post"
 end
-
 configure :build do
 
 end
@@ -12,3 +15,7 @@ activate :s3_sync do |s3_sync|
   s3_sync.bucket = 'spilth.org'
   s3_sync.acl    = 'public-read'
 end
+
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
